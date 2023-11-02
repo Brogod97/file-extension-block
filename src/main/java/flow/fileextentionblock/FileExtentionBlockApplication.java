7ec0e696b63d659fb7b505bdfb8222e00bdf6267
Brogod97 <evergreen1286@gmail.com>
2023-11-02 14:35:01 +0900

    Markup HTML and CSS styling

---
+++ src/main/java/flow/fileextentionblock/FileExtentionBlockApplication.java
@@ -0,0 +1,13 @@
+package flow.fileextentionblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExtentionBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileExtentionBlockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flow/fileextentionblock/FileExtentionBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package flow.fileextentionblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileExtentionBlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
